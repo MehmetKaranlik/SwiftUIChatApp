4f925df802d1bd5beb5845e16913f824537f58c5
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-01-21 03:04:50 +0300

    Created

---
+++ DemoChatApp/DemoChatApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DemoChatApp
+//
+//  Created by mehmet karanlık on 21.01.2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DemoChatApp/DemoChatApp/DemoChatAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoChatAppApp.swift
+//  DemoChatApp
+//
+//  Created by mehmet karanlık on 21.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct DemoChatAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
