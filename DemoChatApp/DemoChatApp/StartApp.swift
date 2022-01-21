9dca9351c5331cd6a31d5b5b13c753c615ac0644
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-01-21 03:08:47 +0300

    First Foldering

--- DemoChatApp/DemoChatApp/StartApp.swift
+++ DemoChatApp/DemoChatApp/StartApp.swift
@@ -8,10 +8,10 @@
 import SwiftUI
 
 @main
-struct DemoChatAppApp: App {
+struct StartApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+         AuthView()
         }
     }
 }
