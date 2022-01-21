7d6285502c26888eba3f59418c8775eec0383b24
Mehmet Karanlık <89414084+MehmetKaranlik@users.noreply.github.com>
2022-01-21 03:09:21 +0300

    ""

---
+++ DemoChatApp/DemoChatApp/View/Auth/View/AuthView.swift
@@ -0,0 +1,20 @@
+//
+//  AuthView.swift
+//  DemoChatApp
+//
+//  Created by mehmet karanlık on 21.01.2022.
+//
+
+import SwiftUI
+
+struct AuthView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AuthView_Previews: PreviewProvider {
+    static var previews: some View {
+        AuthView()
+    }
+}
